6b2f5e31928e49dacc3e6c68f565e752820e4be3
fxjhello <2395112364@qq.com>
2023-04-19 17:24:13 +0800

    2023.4.19

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@midwayjs/core';
-
+import { MainService } from '../service/main.service';
 @Controller('/')
 export class HomeController {
   @Get('/')
   async home(): Promise<string> {
+    const chat = new MainService();
+    chat.run();
     return 'Hello Midwayjs!';
   }
 }
